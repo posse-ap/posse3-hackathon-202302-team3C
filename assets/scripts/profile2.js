5d6ed6f36d9929c6257bb0e13ffeb62cd640758d
inose-yuudai <inobee1412@yahoo.co.jp>
2023-02-25 08:04:39 +0900

    最終調整

--- assets/scripts/profile2.js
+++ assets/scripts/profile2.js
@@ -129,18 +129,204 @@ submitButton.addEventListener('click', function () {
   litag.appendChild(ptag);
   profileBox.appendChild(litag);
   const favoriteLink = document.createElement('a');
-  ptag.innerHTML = '大学名:' + '<span></span>' + universityContent + '<br>' + '学年:' + '<span></span>' + gradesContent + '<br>' + '学部・学科:' + '<span></span>' + facultyContent + '<br>' + 'ふりがな:' + '<span></span>' + subnameContent + '<br>' + '名前:' + '<span></span>' + nameContent + '<br>' + '趣味:' + '<span></span>' + favoriteContent + '<br>' + '診断結果:' + '<span></span>' + sixteenContent + '<br>';
+  ptag.innerHTML = '大学名:' + '<span></span>' + universityContent + '<br>' + '学年:' + '<span></span>' + gradesContent + '<br>' + '学部・学科:' + '<span></span>' + facultyContent + '<br>' + 'ふりがな:' + '<span></span>' + subnameContent + '<br>' + '名前:' + '<span></span>' + nameContent + '<br>' + '趣味:' + '<span></span>' + favoriteContent + '<br>' + '診断結果:' + '<span></span>' + sixteenContent + '<br>'+`<br>`+`<strong>【相性が良い人を見てみよう!】</strong>`+`<br>`;
   if (sixteenContent === sixteenTypes[0]) {
     const compatibleTypeLink = document.createElement('a');
-    compatibleTypeLink.href = `#testkun2`;
-    compatibleTypeLink.textContent = `「${sixteenContent}」と相性が良いのは「${sixteenTypes[12]}」です`;
-    const testkunElement = document.getElementById('testkun2');
-    const testkunElement_self = document.getElementById('testkun3');
+    compatibleTypeLink.href = `#t14`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[14]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t14');
+    const testkunElement_self = document.getElementById('t0');
     testkunElement.style.color = 'red';
     testkunElement_self.style.color = 'red';
     testkunElement_self.classList.add('my-link2');
     ptag.appendChild(compatibleTypeLink);
   }
+  else  if (sixteenContent === sixteenTypes[1]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t13`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[13]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t13');
+    const testkunElement_self = document.getElementById('t1');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[2]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t15`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[15]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t15');
+    const testkunElement_self = document.getElementById('t2');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[3]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t5`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[5]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t5');
+    const testkunElement_self = document.getElementById('t3');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[4]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t6`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[6]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t6');
+    const testkunElement_self = document.getElementById('t4');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[5]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t3`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[3]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t3');
+    const testkunElement_self = document.getElementById('t5');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[6]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t4`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[4]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t4');
+    const testkunElement_self = document.getElementById('t6');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[7]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t11`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[11]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t11');
+    const testkunElement_self = document.getElementById('t7');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+
+  else  if (sixteenContent === sixteenTypes[8]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t9`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[9]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t9');
+    const testkunElement_self = document.getElementById('t8');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[9]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t8`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[8]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t8');
+    const testkunElement_self = document.getElementById('t9');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[10]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t12`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[12]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t12');
+    const testkunElement_self = document.getElementById('t10');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[11]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t7`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[7]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t7');
+    const testkunElement_self = document.getElementById('t11');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[12]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t10`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[10]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t10');
+    const testkunElement_self = document.getElementById('t12');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[13]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t1`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[1]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t1');
+    const testkunElement_self = document.getElementById('t13');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else  if (sixteenContent === sixteenTypes[14]) {
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t0`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[0]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t0');
+    const testkunElement_self = document.getElementById('t14');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+  }
+  else{
+    const compatibleTypeLink = document.createElement('a');
+    compatibleTypeLink.href = `#t2`;
+    compatibleTypeLink.style.color = '#CB98E4';
+    compatibleTypeLink.innerHTML = `<br>`+`「${sixteenContent}」と相性が良いのは「${sixteenTypes[2]}」です。` + `<br>`+`誰がいるか見てみよう！` + `<br>`+`<br>`+`↑これをクリックしてね`+`<br>`;
+    const testkunElement = document.getElementById('t2');
+    const testkunElement_self = document.getElementById('t15');
+    testkunElement.style.color = 'red';
+    testkunElement_self.style.color = 'red';
+    testkunElement_self.classList.add('my-link2');
+    ptag.appendChild(compatibleTypeLink);
+
+  }
+
+
+
 
 
 
@@ -151,11 +337,20 @@ submitButton.addEventListener('click', function () {
   favoriteLink.innerHTML = favoriteContent;
 
 
+  // const options = [
+  //   "プログラミング",
+  //   "料理",
+  //   "ゲーム",
+  //   "旅行",
+  //   "YouTube",
+  //   "K-pop",
+  //   "その他",
+  // ];
 
   if (favoriteContent === options[0]) {
     favoriteLink.href = '../favorite/programming.html';
-  } else if (favoriteContent === options[2]) {
-    favoriteLink.href = 'index.html';
+  } else if (favoriteContent === options[1]) {
+    favoriteLink.href = '../favorite/programming.html';
   } else {
     favoriteLink.href = '#';
   }
